c9bb8ad8b85a378e16c8ec8264041b56e439b8ee
Quarks0 <bdippelsman10@gmail.com>
2016-03-31 00:27:51 -0400

    Started ExpandableListAdapter
    code more or less implemented, need views added, need to understand
    implementation

--- app/src/main/java/com/quarks0/dofusorganizer/ExpandableListAdapter.java
+++ app/src/main/java/com/quarks0/dofusorganizer/ExpandableListAdapter.java
@@ -1,19 +1,99 @@
 package com.quarks0.dofusorganizer;
 
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.content.Context;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by Quarks0 on 3/28/2016.
  */
 public class ExpandableListAdapter extends BaseExpandableListAdapter{
 
-    public Object getGroupView(){
+    private Context context;
+    private List<String> listHeader;
+    private HashMap<String, List<String>> listChild;
 
+    public ExpandableListAdapter(Context _context, List<String> _listHeader, HashMap<String,List<String>> _listChild){
+        this.context = _context;
+        this.listHeader = _listHeader;
+        this.listChild = _listChild;
     }
 
-    public Object getChildView(){
+    @Override
+    public Object getChild(int groupPosition, int childPosition){
+        return this.listChild.get(this.listHeader.get(groupPosition)).get(childPosition);
+    }
 
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition){
+        return childPosition;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent){
+        final String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null){
+            LayoutInflater infalInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.content_harvest_tracker,null);
+        }
+        TextView txtListChild = (TextView) convertView.findViewById(R.id.eternalHarvestList);
+        txtListChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition){
+        return this.listChild.get(this.listHeader.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition){
+        return this.listHeader.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount(){
+        return this.listHeader.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition){
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition,boolean isExpanded,View view, ViewGroup viewGroup){
+        String headerTitle = (String) getGroup(groupPosition);
+
+        if(view == null){
+            LayoutInflater infalInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = infalInflater.inflate(R.layout.content_harvest_tracker,null);
+        }
+
+        TextView labelListHeader = (TextView) view.findViewById(R.id.eternalHarvestList);
+        labelListHeader.setTypeface(null, Typeface.BOLD);
+        labelListHeader.setText(headerTitle);
+
+        return view;
+    }
+
+    @Override
+    public boolean hasStableIds(){
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition){
+        return true;
     }
 }
